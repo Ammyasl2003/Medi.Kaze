fd92e46a1cc10f0c11606fddb6f70804c631c58c
AMANDEEP SINGH MAJITHIA <102940833+Ammyasl2003@users.noreply.github.com>
2025-06-20 13:43:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -222,4 +222,5 @@ def remove(med_id):
     return redirect('/admin')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=10000)
+        app.run(debug=True)
+        app.run(host='0.0.0.0', port=10000)
